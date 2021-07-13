ee4b5d13ec0c58983932d4287a1dd5a9ed4eab79
Jeroen <jeroen@nieuwontwerp.com>
2021-07-13 14:23:53 +0200

    Testing close button #02

--- templates/nav/_nav.js
+++ templates/nav/_nav.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function(){
   jQuery('.search--menu--close--wrapper').click(function(){
     jQuery('.search--menu--results').toggleClass('is--open');
+    jQuery(this).toggleClass('is--clicked');
   });
 });
