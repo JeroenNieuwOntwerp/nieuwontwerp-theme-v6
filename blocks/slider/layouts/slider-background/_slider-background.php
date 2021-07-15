cbd8b149f7f1d443aa6fdfca4b4bb51acffadd03
Jeroen <jeroen@nieuwontwerp.com>
2021-07-15 13:35:31 +0200

    Sliders 0.004

--- blocks/slider/layouts/slider-background/_slider-background.php
+++ blocks/slider/layouts/slider-background/_slider-background.php
@@ -7,11 +7,11 @@ $_button_options__text_colour = $_tile_textcolour;
 $_button_options__url = $_tile_url;
 $_button_options__label = $_tile_label;
 $_button_pseudo = 'true';
-echo '<a href="'. $_tile_url .'" rel="noopener nofollow noreferrer" class="slider-slide-background">
-  <img class="slider-slide--image image-id-'. $_tile_image["ID"] .'" src="' . $_tile_image["value"]["url"] . '" alt="'. $_tile_image["value"]["alt"] . '" />
+echo '<a href="'. $_slide_url .'" rel="noopener nofollow noreferrer" class="slider-slide-background">
+  <img class="slider-slide--image image-id-'. $_slide_image["ID"] .'" src="' . $_slide_image["value"]["url"] . '" alt="'. $_slide_image["value"]["alt"] . '" />
   <div class="slider-slide--overlay" style="background-image: linear-gradient(0deg, '. $_tile_overlaycolour .'80 0%, '. $_tile_overlaycolour .'00 100%);"></div>
-  <span class="slider-slide--content" style="color: '. $_tile_textcolour .' !important;">
-    <h3 class="slider-slide--title">'. $_tile_content .'</h3>';
+  <span class="slider-slide--content" style="color: '. $_slide_textcolour .' !important;">
+    <h3 class="slider-slide--title">'. $_slide_label .'</h3>';
     include(__DIR__ . '/../../../../templates/components/button/_button.php' );
   echo '</span>
 </a>';
--- blocks/slider/layouts/slider-image/_slider-image.php
+++ blocks/slider/layouts/slider-image/_slider-image.php
@@ -8,9 +8,9 @@ $_button_options__url = $_tile_url;
 $_button_options__label = $_tile_label;
 $_button_pseudo = 'true';
 
-echo '<a href="'. $_tile_url .'" rel="noopener nofollow noreferrer" class="slider-slide-image">
-  <img class="slider-slide--image image-id-'. $_tile_image["ID"] .'" src="' . $_tile_image["value"]["url"] . '" alt="'. $_tile_image["value"]["alt"] . '" />
-    <h4 class="slider-slide--image" style="color: '. $_tile_textcolour .' !important;">'. $_tile_content .'</h4>';
+echo '<a href="'. $_slide_url .'" rel="noopener nofollow noreferrer" class="slider-slide-image">
+  <img class="slider-slide--image image-id-'. $_slide_image["ID"] .'" src="' . $_slide_image["value"]["url"] . '" alt="'. $_slide_image["value"]["alt"] . '" />
+    <h4 class="slider-slide--image" style="color: '. $_slide_textcolour .' !important;">'. $_slide_label .'</h4>';
     include(__DIR__ . '/../../../../templates/components/button/_button.php' );
   echo '</span>
 </a>';
