314a2ed4768271c893013fa0623c14109d3ec4fb
Jeroen <jeroen@nieuwontwerp.com>
2021-07-19 15:46:26 +0200

    Attempt 140

--- templates/product-template-a.php
+++ templates/product-template-a.php
@@ -17,7 +17,9 @@ if( have_rows('product--filters', 'options') )
   while( have_rows('product--filters', 'options') )
   {
     the_row();
-    if(get_sub_field('product--filter--for') == get_the_title())
+    $term = get_sub_field('product--filter--for');
+    $_term_string = $term->name;
+    if($_term_string == $_product_family)
     {
       if( have_rows('product--filter--filters') )
       {
