7d749375189a70ab57e8c64e99d41921b6fd0dd8
Jeroen <jeroen@nieuwontwerp.com>
2021-07-19 10:12:21 +0200

    SM_Button Styling 02

--- templates/components/socialmedia-button/_socialmedia-button.php
+++ templates/components/socialmedia-button/_socialmedia-button.php
@@ -5,8 +5,8 @@ defined( 'ABSPATH' ) || exit;
 
 echo '<p class="socialmedia--button">
   <a href="'. $_socialmedia_url .'" target="_blank" rel="noopener nofollow noreferrer">
-    <img class="socialmeduia--button--icon--default" src="'. $_socialmedia_icon_norm["value"]["url"] .'" height="24px" width="24px" alt="'. $_socialmedia_icon_norm["value"]["alt"] .'" title="'. $_socialmedia_icon_norm["value"]["alt"] .'" />
-    <img class="socialmeduia--button--icon--hover" src="'. $_socialmedia_icon_hover["value"]["url"] .'" height="24px" width="24px" alt="'. $_socialmedia_icon_hover["value"]["alt"] .'" title="'. $_socialmedia_icon_hover["value"]["alt"] .'" />
+    <img class="socialmedia--button--icon--default" src="'. $_socialmedia_icon_norm["value"]["url"] .'" height="24px" width="24px" alt="'. $_socialmedia_icon_norm["value"]["alt"] .'" title="'. $_socialmedia_icon_norm["value"]["alt"] .'" />
+    <img class="socialmedia--button--icon--hover" src="'. $_socialmedia_icon_hover["value"]["url"] .'" height="24px" width="24px" alt="'. $_socialmedia_icon_hover["value"]["alt"] .'" title="'. $_socialmedia_icon_hover["value"]["alt"] .'" />
   </a>
 </p>';
 
