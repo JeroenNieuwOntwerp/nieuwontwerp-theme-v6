34c12dc126c1b76259abbf77ec4bb97eb736c1e3
Jeroen <jeroen@nieuwontwerp.com>
2021-07-19 09:10:33 +0200

    Adding Footer options-page

--- functions/_acf_option_pages.php
+++ functions/_acf_option_pages.php
@@ -10,6 +10,14 @@ function no_acf_option_pages() {
           'capability'    => 'edit_posts',
           'redirect'      => false
       ));
+
+      $option_page = acf_add_options_page(array(
+          'page_title'    => __('Footer'),
+          'menu_title'    => __('Footer'),
+          'menu_slug'     => 'footer-settings',
+          'capability'    => 'edit_posts',
+          'redirect'      => false
+      ));
 };
 
 ?>
