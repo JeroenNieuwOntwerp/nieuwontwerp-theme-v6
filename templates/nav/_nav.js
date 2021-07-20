df4ebae074deecd2120e042b661293bc72e3e534
Jeroen <jeroen@nieuwontwerp.com>
2021-07-20 09:31:19 +0200

    More nav styling

--- templates/nav/_nav.js
+++ templates/nav/_nav.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
   $(document).on('click', '.search--menu--close--wrapper', function()
   {
-    jQuery('.search--menu--results').toggleClass('is--open');
+    $('.search--menu--results').toggleClass('is--open');
+  });
+
+  $(document).on('hover', '.mega-has-dropdown', function()
+  {
+    $('.nav--bars--lower').toggleClass('dropdown-is-active');
   });
 });
