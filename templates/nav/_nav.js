2a5d249e1fef89d2882234cc0c00b66498c35dc5
Jeroen <jeroen@nieuwontwerp.com>
2021-07-20 09:35:23 +0200

    js not working?

--- templates/nav/_nav.js
+++ templates/nav/_nav.js
@@ -8,4 +8,9 @@ $(document).ready(function(){
   {
     $('.nav--bars--lower').toggleClass('dropdown-is-active');
   });
+
+  $('.mega-has-dropdown').hover(function()
+  {
+    $('.nav--bars--lower').toggleClass('dropdown-is-active');
+  });
 });
