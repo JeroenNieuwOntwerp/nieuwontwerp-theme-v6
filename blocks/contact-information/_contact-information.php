f808175993e57d05527f4f67c3d2ed04d1cbee04
Jeroen <jeroen@nieuwontwerp.com>
2021-07-26 15:50:17 +0200

    26072021 #016

--- blocks/contact-information/_contact-information.php
+++ blocks/contact-information/_contact-information.php
@@ -5,7 +5,7 @@ $_row_id = get_row_index();
 $_options__margin_top = get_sub_field('margin-top');
 $_options__margin_bottom = get_sub_field('margin-bottom');
 $_options__style = get_sub_field('style');
-echo '<section class="contact-information flex row-id-'. $_row_id .' '. $_options__style .'" style="margin-top: '. $_options__margin_top .'px !important; margin-bottom: '. $_options__margin_bottom .'px !important;">';
+echo '<section class="contact-information flex row-id-'. $_row_id .' style--'. $_options__style .'" style="margin-top: '. $_options__margin_top .'px !important; margin-bottom: '. $_options__margin_bottom .'px !important;">';
 while( have_rows('contact-information--contacts') ) :
   the_row();
   $_post = get_sub_field('contact-information--contact');
