fde7e4936e0eca61de2ffe74f1494307291b4c17
Jeroen <jeroen@nieuwontwerp.com>
2021-07-27 13:23:26 +0200

    27-07-2021 #006

--- blocks/form/_form.php
+++ blocks/form/_form.php
@@ -3,9 +3,9 @@
 defined( 'ABSPATH' ) || exit;
 
 $_row_id = get_row_index();
-$_id = get_sub_field('id');
+$_id = get_sub_field('form--form-id');
 //
-echo '<section class="no-form no-form-id-'. $_id .'">
+echo '<section class="form row-id-'. $_row_id .'">
   ' . do_shortcode("[ninja_form id=" . $_id . "]") . '
 </section>';
 ?>
