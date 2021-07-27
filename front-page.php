75e3209805367a031a18de88af793878b1936227
Jeroen <jeroen@nieuwontwerp.com>
2021-07-27 12:43:16 +0200

    27-07-2021 #004

--- front-page.php
+++ front-page.php
@@ -12,6 +12,21 @@ while( have_rows('flex-content') ): the_row();
   $_block = 'blocks/'.$variable.'/_'.$variable.'.php';
   include($_block);
 endwhile;
-echo '</main>';
+echo '</main>
+<style>
+  .row-id-'. get_field('black--background') .'::before
+  {
+    content: "";
+    display: block;
+    height: 865px;
+    width: 100%;
+    position: absolute;
+    top: -150px;
+    left: 0px;
+    right: 0px;
+    z-index: -5;
+    background-color: #202020;
+  }
+</style>';
 include_once('templates/footer/_footer.php');
 ?>
