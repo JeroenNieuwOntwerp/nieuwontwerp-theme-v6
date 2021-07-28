35079e312cb615f47d43085cd06e52b2782c2bed
Jeroen <jeroen@nieuwontwerp.com>
2021-07-28 10:52:28 +0200

    28-07-2021 #002

--- templates/components/archive-item/recent-news/_recent-news.php
+++ templates/components/archive-item/recent-news/_recent-news.php
@@ -1,6 +1,19 @@
 <?php
 defined( 'ABSPATH' ) || exit;
 
-echo '<div><h2>NIEUWSJE</h2></div>';
+$_button_options__ID = get_the_ID($post);
+$_button_options__style = 'transparent';
+$_button_options__text_colour = '#202020';
+$_button_options__url = get_the_permalink($post);
+$_button_options__label = 'Read article';
+$_button_pseudo = 'true';
+
+echo '<div class="recent-project-card">
+  <h2>NIEUWS</h2>
+  <span class="content">
+  ';
+  include(__DIR__ . '/../../button/_button.php' );
+  echo '</span>
+</div>';
 
 ?>
--- templates/components/archive-item/recent-project/_recent-project.php
+++ templates/components/archive-item/recent-project/_recent-project.php
@@ -1,4 +1,19 @@
 <?php
 defined( 'ABSPATH' ) || exit;
 
+$_button_options__ID = get_the_ID($post);
+$_button_options__style = 'transparent';
+$_button_options__text_colour = '#202020';
+$_button_options__url = get_the_permalink($post);
+$_button_options__label = 'Read project';
+$_button_pseudo = 'true';
+
+echo '<div class="recent-project-card">
+  <h2>PROJECT</h2>
+  <span class="content">
+  ';
+  include(__DIR__ . '/../../button/_button.php' );
+  echo '</span>
+</div>';
+
 ?>
